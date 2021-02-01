137445b87177e672708b25f47ed58a97cfc7242d
Aleksey <aleks4ay@gmail.com>
2021-02-01 23:03:14 +0200

    Spring MVC CRUD application (with JPA autogeneration ddl). Rus symbol INVALID.

---
+++ src/main/java/ua/aser/customer/CustomerRepository.java
@@ -0,0 +1,17 @@
+package ua.aser.customer;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CustomerRepository extends CrudRepository<Customer, Long>{
+    @Query(value = "SELECT c FROM Customer c WHERE c.name LIKE '%' || :keyword || '%'"
+            + " OR c.email LIKE '%' || :keyword || '%'"
+            + " OR c.address LIKE '%' || :keyword || '%'")
+    public List<Customer> search(@Param("keyword") String keyword);
+
+}
